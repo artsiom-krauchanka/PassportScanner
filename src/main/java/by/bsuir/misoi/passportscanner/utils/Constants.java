387d1af8d3e4c7ad66046b57a18a1accef957f2a
swaggalolz <rosmen1@tut.by>
2016-09-23 22:46:44 +0300

    cleanup

--- src/main/java/by/bsuir/misoi/passportscanner/utils/Constants.java
+++ src/main/java/by/bsuir/misoi/passportscanner/utils/Constants.java
@@ -1,13 +1,5 @@
 package by.bsuir.misoi.passportscanner.utils;
 
-import by.bsuir.misoi.passportscanner.filters.Filter;
-import by.bsuir.misoi.passportscanner.filters.MedianFilter;
-import by.bsuir.misoi.passportscanner.filters.MonochromeFilter;
-import by.bsuir.misoi.passportscanner.filters.ReduceNoiseFilter;
-import com.google.common.collect.ImmutableList;
-
-import java.util.List;
-
 public class Constants {
 
     private Constants() {}
