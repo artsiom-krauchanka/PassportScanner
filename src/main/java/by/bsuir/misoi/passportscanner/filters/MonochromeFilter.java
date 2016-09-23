db622bed65bbd870a1c2b4a675122fa95c0527c4
swaggalolz <rosmen1@tut.by>
2016-09-23 22:45:57 +0300

    new logic

--- src/main/java/by/bsuir/misoi/passportscanner/filters/MonochromeFilter.java
+++ src/main/java/by/bsuir/misoi/passportscanner/filters/MonochromeFilter.java
@@ -1,23 +1,36 @@
 package by.bsuir.misoi.passportscanner.filters;
 
+import by.bsuir.misoi.passportscanner.utils.ColorRGB;
+
 public class MonochromeFilter implements Filter {
 
-    private final int multiplier;
+    private final int threshold;
 
     public MonochromeFilter() {
-        this.multiplier = 0xFFFFF;
+        this.threshold = 150;
     }
 
-    public MonochromeFilter(int multiplier) {
-        this.multiplier = multiplier;
+    public MonochromeFilter(int threshold) {
+        this.threshold = threshold;
     }
 
     @Override
     public int[] transform(int width, int height, int[] pixels) {
         final int[] transform = new int[width * height];
+
         for (int i = 0; i < width * height; i++) {
-            transform[i] = pixels[i] & multiplier;
+
+            int red = ColorRGB.getRed(pixels[i]);
+            int green = ColorRGB.getGreen(pixels[i]);
+            int blue = ColorRGB.getBlue(pixels[i]);
+
+
+            transform[i] = (red+green+blue)/3 < threshold ? ColorRGB.getBlackColor() : ColorRGB.getWhiteColor();
+
         }
+
         return transform;
     }
+
+
 }
