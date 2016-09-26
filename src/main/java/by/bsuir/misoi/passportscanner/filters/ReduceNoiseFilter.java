a324c2ef46ddaf71a3eb22a4f7fbb904da4730d3
Артём Кравченко <artemkravchenko1994@gmail.com>
2016-09-26 11:35:28 +0300

    Reduse Noise Filter refactoring -add configurable window size like in Median Filter.

--- src/main/java/by/bsuir/misoi/passportscanner/filters/ReduceNoiseFilter.java
+++ src/main/java/by/bsuir/misoi/passportscanner/filters/ReduceNoiseFilter.java
@@ -2,30 +2,38 @@ package by.bsuir.misoi.passportscanner.filters;
 
 public class ReduceNoiseFilter implements Filter {
 
-    public ReduceNoiseFilter() {}
+    private final int FILTER_SIZE;
+
+    public ReduceNoiseFilter() {
+        FILTER_SIZE = 3;
+    }
+
+    public ReduceNoiseFilter(int filterSize) {
+        this.FILTER_SIZE = filterSize;
+    }
 
     @Override
     public int[] transform(int width, int height, int[] pixels) {
         int index = 0;
-        final int[] r = new int[9];
-        final int[] g = new int[9];
-        final int[] b = new int[9];
+        final int[] r = new int[FILTER_SIZE * FILTER_SIZE];
+        final int[] g = new int[FILTER_SIZE * FILTER_SIZE];
+        final int[] b = new int[FILTER_SIZE * FILTER_SIZE];
         final int[] outPixels = new int[width * height];
 
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
                 int k = 0;
-                int irgb = pixels[index];
+                int irgb = pixels[index];       //current pixel
                 int ir = (irgb >> 16) & 0xff;
                 int ig = (irgb >> 8) & 0xff;
                 int ib = irgb & 0xff;
 
-                for (int dy = -1; dy <= 1; dy++) {
+                for (int dy = -FILTER_SIZE/2; dy <= FILTER_SIZE/2; dy++) {
                     int iy = y+dy;
                     if (0 <= iy && iy < height) {
-                        int ioffset = iy*width;
+                        int ioffset = iy*width;         //offset in the pixel array (in orde rto find pixels in array)
 
-                        for (int dx = -1; dx <= 1; dx++) {
+                        for (int dx = -FILTER_SIZE/2; dx <= FILTER_SIZE/2; dx++) {
                             int ix = x+dx;
                             if (0 <= ix && ix < width) {
                                 int rgb = pixels[ioffset+ix];
@@ -40,7 +48,7 @@ public class ReduceNoiseFilter implements Filter {
                             k++;
                         }
                     } else {
-                        for (int dx = -1; dx <= 1; dx++) {
+                        for (int dx = -FILTER_SIZE/2; dx <= FILTER_SIZE/2; dx++) {
                             r[k] = ir;
                             g[k] = ig;
                             b[k] = ib;
@@ -58,8 +66,8 @@ public class ReduceNoiseFilter implements Filter {
     private int smooth(int[] v) {
         int minIndex = 0, maxIndex = 0, min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
 
-        for (int i = 0; i < 9; i++) {
-            if ( i != 4 ) {
+        for (int i = 0; i < FILTER_SIZE * FILTER_SIZE; i++) {
+            if ( i != FILTER_SIZE / 2 ) {
                 if (v[i] < min) {
                     min = v[i];
                     minIndex = i;
@@ -70,10 +78,10 @@ public class ReduceNoiseFilter implements Filter {
                 }
             }
         }
-        if ( v[4] < min )
+        if ( v[FILTER_SIZE / 2] < min )
             return v[minIndex];
-        if ( v[4] > max )
+        if ( v[FILTER_SIZE / 2] > max )
             return v[maxIndex];
-        return v[4];
+        return v[FILTER_SIZE / 2];
     }
 }
